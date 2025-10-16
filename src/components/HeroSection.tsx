c39566253802f449462857759b2939d2aefed38c
Vivek Singh <vividrajput@gmail.com>
2025-10-17 03:59:19 +0530

    Add Poppins font and remove carousel controls
    - Replaced Geist fonts with Poppins font family
    - Added multiple font weights (300-900) for Poppins
    - Created Tailwind config to set Poppins as default font
    - Removed country carousel buttons and indicators
    - Maintained auto-rotation functionality

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -165,41 +165,6 @@ export default function HeroSection() {
         </div>
       </div>
 
-      {/* Country Carousel */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10">
-        <div className="flex space-x-4 bg-white bg-opacity-20 backdrop-blur-sm rounded-full px-6 py-3">
-          {countries.map((country, index) => (
-            <button
-              key={country.name}
-              onClick={() => handleCountrySelect(index)}
-              className={`px-4 py-2 rounded-full text-sm font-medium transition-all duration-300 ${
-                index === currentCountryIndex
-                  ? 'bg-white text-gray-900 shadow-lg'
-                  : 'text-white hover:bg-white hover:bg-opacity-20'
-              }`}
-            >
-              {country.name}
-            </button>
-          ))}
-        </div>
-      </div>
-
-      {/* Carousel Indicators */}
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10">
-        <div className="flex space-x-2">
-          {countries.map((_, index) => (
-            <button
-              key={index}
-              onClick={() => handleCountrySelect(index)}
-              className={`w-3 h-3 rounded-full transition-all duration-300 ${
-                index === currentCountryIndex
-                  ? 'bg-white'
-                  : 'bg-white bg-opacity-50 hover:bg-opacity-75'
-              }`}
-            />
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
