aa6df4ff58a5e400b8318e7410433065f1aa9fb3
Vivek Singh <vividrajput@gmail.com>
2025-10-17 06:17:35 +0530

    Revert "Update Europe packages section: reduce font size, add rounded button, adjust spacing and container width"
    This reverts commit e0987fc4b61fcbb8969841e3284537bb3fb656be.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import HeroSection from '@/components/HeroSection';
 import DestinationCards from '@/components/DestinationCards';
-import EuropePackages from '@/components/EuropePackages';
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <HeroSection />
       <DestinationCards />
-      <EuropePackages />
     </div>
   );
 }
